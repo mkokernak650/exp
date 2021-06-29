f60eb6de76b2f406d91e02aa5d7e6d9a76cb4392
mdabbas-cse <gmabbas44@gmail.com>
2021-06-29 19:54:23 +0600

    create ringba call log table, user table with migration

---
+++ database/factories/RingCallLogFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\RingCallLog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RingCallLogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = RingCallLog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
