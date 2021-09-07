fb26929c4386a53953d3afae96851e5a73c9bd50
mdabbas-cse <gmabbas44@gmail.com>
2021-09-07 12:25:55 +0600

    Update BroadCastMonthController.php

--- app/Http/Controllers/BroadCastMonthController.php
+++ app/Http/Controllers/BroadCastMonthController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\BroadCastMonth;
-use Illuminate\Support\Facades\DB;
 
 class BroadCastMonthController extends Controller
 {
-
     function __construct()
     {
         $this->middleware('auth');
@@ -21,9 +19,12 @@ class BroadCastMonthController extends Controller
 
     public function store(Request $request)
     {
-   
-        $existData = BroadCastMonth::where('broad_cast_month', $request->broad_cast_month)->where('start_date', $request->start_date)->where('end_date', $request->end_date)->count();
-        if ($existData>0) {
+        $existData = BroadCastMonth::where([
+            'broad_cast_month' => $request->broad_cast_month,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date
+        ])->count();
+        if ($existData > 0) {
             return response()->json(["msg" => "Data already exists"]);
         }
         BroadCastMonth::create([
@@ -36,7 +37,6 @@ class BroadCastMonthController extends Controller
 
     public function broadCastMonthReport()
     {
-      
         $BroadCastMonths = BroadCastMonth::all();
         return Inertia::render('Settings/BroadcastMonthReport', [
             'BroadCastMonths' => $BroadCastMonths,
@@ -45,17 +45,23 @@ class BroadCastMonthController extends Controller
 
     public function delete(Request $request)
     {
-        $result = true;
+        $result = false;
+        $ids = $request->selectedRowIds;
         $i = 0;
-        while ($i < count($request->selectedRowIds)) {
-            $result =  DB::table('broad_cast_months')->where('id', $request->selectedRowIds[$i])->delete();
+        while ($i < count($ids)) {
+            $result =  BroadCastMonth::where('id', $request->selectedRowIds[$i])->delete();
             $i++;
         }
         if ($result) {
-            return response()->json(["msg" => "Successfully Deleted", "status_code" => 200]);
-        }
-        if ($result) {
-            return response()->json(["msg" => "Deleting Failed", "status_code" => 500]);
+            return response()->json([
+                "msg" => "Successfully Deleted",
+                "status_code" => 200
+            ]);
+        } else {
+            return response()->json([
+                "msg" => "Deleting Failed",
+                "status_code" => 500
+            ]);
         }
     }
 }
