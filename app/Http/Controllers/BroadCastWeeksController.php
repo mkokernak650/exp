8ac3bf7c37422bdb6b0f33d833faa1b328db2e74
mdabbas-cse <gmabbas44@gmail.com>
2021-09-07 12:36:39 +0600

    Update BroadCastWeeksController.php

--- app/Http/Controllers/BroadCastWeeksController.php
+++ app/Http/Controllers/BroadCastWeeksController.php
@@ -14,6 +14,7 @@ class BroadCastWeeksController extends Controller
     {
         $this->middleware('auth');
     }
+
     public function index()
     {
         return Inertia::render('Settings/AddBroadcastWeek');
@@ -21,25 +22,28 @@ class BroadCastWeeksController extends Controller
 
     public function store(Request $request)
     {
-   
-        $existData = BroadCastWeeks::where('broad_cast_week', $request->broad_cast_week)->where('start_date', $request->start_date)->where('end_date', $request->end_date)->count();
-        if ($existData>0) {
+        $existData = BroadCastWeeks::where([
+            'broad_cast_week'   => $request->broad_cast_week,
+            'start_date'        => $request->start_date,
+            'end_date'          => $request->end_date
+        ])->count();
+        if ($existData > 0) {
             return response()->json(["msg" => "Data already exists"]);
         }
         BroadCastWeeks::create([
-            'broad_cast_week' => $request->broad_cast_week,
-            'start_date' => $request->start_date,
-            'end_date' => $request->end_date,
+            'broad_cast_week'   => $request->broad_cast_week,
+            'start_date'        => $request->start_date,
+            'end_date'          => $request->end_date,
         ]);
         return response()->json(["msg" => "Successfully Added"]);
     }
 
     public function broadCastWeekReport()
     {
-      
+
         $BroadCastWeeks = BroadCastWeeks::all();
         return Inertia::render('Settings/BroadcastWeekReport', [
-            'BroadCastWeeks' => $BroadCastWeeks,
+            'BroadCastWeeks'    => $BroadCastWeeks,
         ]);
     }
 
@@ -48,14 +52,21 @@ class BroadCastWeeksController extends Controller
         $result = true;
         $i = 0;
         while ($i < count($request->selectedRowIds)) {
-            $result =  DB::table('broad_cast_weeks')->where('id', $request->selectedRowIds[$i])->delete();
+            // $result =  DB::table('broad_cast_weeks')->where('id', $request->selectedRowIds[$i])->delete();
+            $result =  BroadCastWeeks::where('id', $request->selectedRowIds[$i])->delete();
             $i++;
         }
         if ($result) {
-            return response()->json(["msg" => "Successfully Deleted", "status_code" => 200]);
+            return response()->json([
+                "msg"           => "Successfully Deleted",
+                "status_code"   => 200
+            ]);
         }
         if ($result) {
-            return response()->json(["msg" => "Deleting Failed", "status_code" => 500]);
+            return response()->json([
+                "msg"           => "Deleting Failed",
+                "status_code"   => 500
+            ]);
         }
     }
 }
