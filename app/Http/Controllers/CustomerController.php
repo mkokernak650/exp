92431d2460c2e6b911a169570e21887ba7317f58
mdabbas-cse <gmabbas44@gmail.com>
2021-09-07 13:45:36 +0600

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Models\Customer;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
@@ -72,16 +71,16 @@ class CustomerController extends Controller
 
     public function delete(Request $request)
     {
-        $result = true;
+        $result = false;
         $i = 0;
         while ($i < count($request->selectedRowIds)) {
-            $result =  DB::table('customers')->where('id', $request->selectedRowIds[$i])->delete();
+            // $result =  DB::table('customers')->where('id', $request->selectedRowIds[$i])->delete();
+            $result =  Customer::where('id', $request->selectedRowIds[$i])->delete();
             $i++;
         }
         if ($result) {
             return response()->json(["msg" => "Successfully Deleted", "status_code" => 200]);
-        }
-        if ($result) {
+        } else {
             return response()->json(["msg" => "Deleting Failed", "status_code" => 500]);
         }
     }
