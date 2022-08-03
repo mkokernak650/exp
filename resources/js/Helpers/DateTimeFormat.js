165d11d93b3ceed528b0e1cf82bf30be7dc7e71e
Shakhawat Hosen <mdshakhawathosen122@gmail.com>
2022-08-03 16:59:49 +0600

    create datetimeformat global function

---
+++ resources/js/Helpers/DateTimeFormat.js
@@ -0,0 +1,11 @@
+export const DateTimeFormat = (value) => {
+    let d = new Date(value)
+    let hours = d.getHours()
+    let minutes = d.getMinutes()
+    let ampm = hours >= 12 ? "PM" : "AM"
+    hours = hours % 12
+    hours = hours ? hours : 12 // the hour "0" should be "12"
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    let strTime = hours + ":" + minutes + " " + ampm
+    return d.getDate() + "-" + new Intl.DateTimeFormat('en', { month: 'short' }).format(d) + "-" + d.getFullYear().toString().substr(-2) + " " + strTime
+}
