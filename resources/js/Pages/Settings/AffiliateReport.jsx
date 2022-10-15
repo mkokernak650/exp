51949ce92077c1e6c4fd5ea64b1a25545d7aef19
Fahim Sakib <fsakib991@gmail.com>
2022-10-15 14:53:12 +0600

    Affiliate section: Helper files added

--- resources/js/Pages/Settings/AffiliateReport.jsx
+++ resources/js/Pages/Settings/AffiliateReport.jsx
@@ -1,7 +1,7 @@
 import Layout from '../Layout/Layout'
 import React, { useEffect, useState, useRef } from 'react'
 import { kaReducer, Table } from 'ka-table'
-import { DataType, SortingMode, PagingPosition } from 'ka-table/enums'
+import { SortingMode, PagingPosition } from 'ka-table/enums'
 import { kaPropsUtils } from 'ka-table/utils'
 import { usePage } from '@inertiajs/inertia-react'
 import FilterControl from 'react-filter-control'
@@ -15,7 +15,7 @@ import Tooltip from '@material-ui/core/Tooltip'
 import DeleteIcon from '@material-ui/icons/Delete'
 import IconButton from '@material-ui/core/IconButton'
 import TextField from '@material-ui/core/TextField'
-import { Button, makeStyles } from '@material-ui/core'
+import { Button } from '@material-ui/core'
 import axios from 'axios'
 import { Helmet } from 'react-helmet'
 import ConfirmModal from '@/Shared/ConfirmModal'
@@ -26,106 +26,7 @@ import addTableDetails from '@/Helpers/AddTableDetails'
 import SelectionHeader from '@/Components/TableComponents/SelectionHeader'
 import SelectionCell from '@/Components/TableComponents/SelectionCell'
 import handleSelects from '@/Helpers/HandleSelects'
-
-const useStyles = makeStyles(() => ({
-  topBtn: {
-    display: 'flex',
-    gap: '10px',
-    marginLeft: '10px',
-  },
-  button: {
-    width: 130,
-    textTransform: 'capitalize',
-    fontSize: '14px',
-  },
-  editButton: {
-    marginTop: '15px',
-  },
-}))
-
-const operators = [
-  {
-    caption: 'Contains',
-    name: 'contains',
-  },
-  {
-    caption: 'Not Contains',
-    name: 'doesNotContain',
-  },
-  {
-    caption: 'Is Empty',
-    name: 'isEmpty',
-  },
-  {
-    caption: 'Is Not Empty',
-    name: 'isNotEmpty',
-  },
-  {
-    caption: 'Starts With',
-    name: 'startswith',
-  },
-  {
-    caption: 'Ends With',
-    name: 'endsWith',
-  },
-  {
-    caption: 'Is',
-    name: 'is',
-  },
-  {
-    caption: 'Is Not',
-    name: 'isnot',
-  },
-]
-
-export const fields = [
-  {
-    caption: 'affiliate_id',
-    name: 'affiliate_id',
-    operators,
-  },
-  {
-    caption: 'affiliate_name',
-    name: 'affiliate_name',
-    operators,
-  },
-  {
-    caption: 'email',
-    name: 'email',
-    operators,
-  },
-  {
-    caption: 'telephone',
-    name: 'telephone',
-    operators,
-  },
-  {
-    caption: 'address',
-    name: 'address',
-    operators,
-  },
-]
-
-export const groups = [
-  {
-    caption: 'And',
-    name: 'and',
-  },
-  {
-    caption: 'Or',
-    name: 'or',
-  },
-]
-
-export const filter = {
-  groupName: 'and',
-  items: [
-    {
-      field: 'affiliate_id',
-      operator: 'isNotEmpty',
-    },
-  ],
-}
+import { useStyles, fields, groups, filter, columns } from './Helpers/AffiliateReportProps'
 
 const AffiliateReport = () => {
   const classes = useStyles()
@@ -151,54 +52,6 @@ const AffiliateReport = () => {
     id: item.id,
     key: index,
   }))
-
-  const columns = [
-    {
-      key: 'edit',
-      style: { width: 20 },
-      visible: true,
-    },
-    {
-      key: 'selection-cell',
-      style: { width: 80 },
-      visible: true,
-    },
-    {
-      key: 'affiliate_id',
-      title: 'Affiliate Id',
-      dataType: DataType.String,
-      style: { width: 350 },
-      visible: true,
-    },
-    {
-      key: 'affiliate_name',
-      title: 'Affiliate Name',
-      dataType: DataType.String,
-      style: { width: 240 },
-      visible: true,
-    },
-    {
-      key: 'email',
-      title: 'Email',
-      dataType: DataType.String,
-      style: { width: 350 },
-      visible: true,
-    },
-    {
-      key: 'telephone',
-      title: 'Telephone',
-      dataType: DataType.String,
-      style: { width: 150 },
-      visible: true,
-    },
-    {
-      key: 'address',
-      title: 'Address',
-      dataType: DataType.String,
-      style: { width: 240 },
-      visible: true,
-    },
-  ]
 
   const optionKey = 'affiliate-report'
   const [columnDetails, setColumnDetails] = useState(
@@ -268,6 +121,7 @@ const AffiliateReport = () => {
   const handleColumns = () => {
     setShowColumns(true)
   }
+  
   const closeSidebar = () => {
     setSearchSidebar(false)
   }
