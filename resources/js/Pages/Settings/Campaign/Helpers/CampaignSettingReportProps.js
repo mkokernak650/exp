31d35358e6506282fc10f2f6729a4d02015289de
Fahim Sakib <fsakib991@gmail.com>
2022-10-15 11:54:26 +0600

    Pay per call setup section: Helper files added

---
+++ resources/js/Pages/Settings/Campaign/Helpers/CampaignSettingReportProps.js
@@ -0,0 +1,198 @@
+import { makeStyles } from '@material-ui/core'
+import { DataType } from 'ka-table/enums'
+
+export const useStyles = makeStyles(() => ({
+  topBtn: {
+    display: 'flex',
+    gap: '10px',
+    marginLeft: '10px',
+  },
+  button: {
+    width: 130,
+    textTransform: 'capitalize',
+    fontSize: '14px',
+  },
+  editButton: {
+    marginTop: '15px',
+  },
+}))
+
+export const fields = [
+  {
+    caption: 'campaign',
+    name: 'campaign',
+    operators: [
+      {
+        caption: 'Contains',
+        name: 'contains',
+      },
+      {
+        caption: 'Not Contains',
+        name: 'doesNotContain',
+      },
+      {
+        caption: 'Is Empty',
+        name: 'isEmpty',
+      },
+      {
+        caption: 'Is Not Empty',
+        name: 'isNotEmpty',
+      },
+      {
+        caption: 'Starts With',
+        name: 'startswith',
+      },
+      {
+        caption: 'Ends With',
+        name: 'endsWith',
+      },
+      {
+        caption: 'Is',
+        name: 'is',
+      },
+      {
+        caption: 'Is Not',
+        name: 'isnot',
+      },
+    ],
+  },
+  {
+    caption: 'duration',
+    name: 'duration',
+    operators: [
+      {
+        caption: 'Contains',
+        name: 'contains',
+      },
+      {
+        caption: 'Not Contains',
+        name: 'doesNotContain',
+      },
+      {
+        caption: 'Is Empty',
+        name: 'isEmpty',
+      },
+      {
+        caption: 'Is Not Empty',
+        name: 'isNotEmpty',
+      },
+      {
+        caption: 'Starts With',
+        name: 'startswith',
+      },
+      {
+        caption: 'Ends With',
+        name: 'endsWith',
+      },
+      {
+        caption: 'Is',
+        name: 'is',
+      },
+      {
+        caption: 'Is Not',
+        name: 'isnot',
+      },
+    ],
+  },
+  {
+    caption: 'status',
+    name: 'status',
+    operators: [
+      {
+        caption: 'Contains',
+        name: 'contains',
+      },
+      {
+        caption: 'Not Contains',
+        name: 'doesNotContain',
+      },
+      {
+        caption: 'Is Empty',
+        name: 'isEmpty',
+      },
+      {
+        caption: 'Is Not Empty',
+        name: 'isNotEmpty',
+      },
+      {
+        caption: 'Starts With',
+        name: 'startswith',
+      },
+      {
+        caption: 'Ends With',
+        name: 'endsWith',
+      },
+      {
+        caption: 'Is',
+        name: 'is',
+      },
+      {
+        caption: 'Is Not',
+        name: 'isnot',
+      },
+    ],
+  },
+]
+
+export const groups = [
+  {
+    caption: 'And',
+    name: 'and',
+  },
+  {
+    caption: 'Or',
+    name: 'or',
+  },
+]
+
+export const filter = {
+  groupName: 'and',
+  items: [
+    {
+      field: 'campaign',
+      operator: 'isNotEmpty',
+    },
+  ],
+}
+
+export const columns = [
+  {
+    key: 'selection-cell',
+    style: { width: 60 },
+    visible: true,
+  },
+  {
+    key: 'sl',
+    title: 'SL',
+    dataType: DataType.Number,
+    style: { width: 40 },
+    visible: false,
+  },
+  {
+    key: 'campaign',
+    title: 'Campaign',
+    dataType: DataType.String,
+    style: { width: 240 },
+    visible: true,
+  },
+  {
+    key: 'duration',
+    title: 'Connection Duration',
+    dataType: DataType.String,
+    style: { width: 100 },
+    visible: true,
+  },
+  {
+    key: 'status',
+    title: 'Status',
+    dataType: DataType.String,
+    style: { width: 100 },
+    visible: true,
+  },
+  {
+    key: 'actions',
+    title: 'Actions',
+    style: { width: 250 },
+    visible: true,
+  },
+]
---
+++ resources/js/Pages/Settings/Helpers/MarketExceptionReportProps.js
@@ -0,0 +1,264 @@
+import { makeStyles } from '@material-ui/core'
+import { DataType } from 'ka-table/enums'
+
+export const useStyles = makeStyles(() => ({
+  topBtn: {
+    display: 'flex',
+    gap: '10px',
+    marginLeft: '10px',
+  },
+  button: {
+    width: 130,
+    textTransform: 'capitalize',
+    fontSize: '14px',
+  },
+  editButton: {
+    marginTop: '15px',
+  },
+}))
+
+export const fields = [
+  {
+    caption: ' campaign',
+    name: 'campaign',
+    operators: [
+      {
+        caption: 'Contains',
+        name: 'contains',
+      },
+      {
+        caption: 'Not Contains',
+        name: 'doesNotContain',
+      },
+      {
+        caption: 'Is Empty',
+        name: 'isEmpty',
+      },
+      {
+        caption: 'Is Not Empty',
+        name: 'isNotEmpty',
+      },
+      {
+        caption: 'Starts With',
+        name: 'startswith',
+      },
+      {
+        caption: 'Ends With',
+        name: 'endsWith',
+      },
+      {
+        caption: 'Is',
+        name: 'is',
+      },
+      {
+        caption: 'Is Not',
+        name: 'isnot',
+      },
+    ],
+  },
+  {
+    caption: 'market',
+    name: 'market',
+    operators: [
+      {
+        caption: 'Contains',
+        name: 'contains',
+      },
+      {
+        caption: 'Not Contains',
+        name: 'doesNotContain',
+      },
+      {
+        caption: 'Is Empty',
+        name: 'isEmpty',
+      },
+      {
+        caption: 'Is Not Empty',
+        name: 'isNotEmpty',
+      },
+      {
+        caption: 'Starts With',
+        name: 'startswith',
+      },
+      {
+        caption: 'Ends With',
+        name: 'endsWith',
+      },
+      {
+        caption: 'Is',
+        name: 'is',
+      },
+      {
+        caption: 'Is Not',
+        name: 'isnot',
+      },
+    ],
+  },
+  {
+    caption: 'state',
+    name: 'state',
+    operators: [
+      {
+        caption: 'Contains',
+        name: 'contains',
+      },
+      {
+        caption: 'Not Contains',
+        name: 'doesNotContain',
+      },
+      {
+        caption: 'Is Empty',
+        name: 'isEmpty',
+      },
+      {
+        caption: 'Is Not Empty',
+        name: 'isNotEmpty',
+      },
+      {
+        caption: 'Starts With',
+        name: 'startswith',
+      },
+      {
+        caption: 'Ends With',
+        name: 'endsWith',
+      },
+      {
+        caption: 'Is',
+        name: 'is',
+      },
+      {
+        caption: 'Is Not',
+        name: 'isnot',
+      },
+    ],
+  },
+  {
+    caption: 'call_type',
+    name: 'call_type',
+    operators: [
+      {
+        caption: 'Contains',
+        name: 'contains',
+      },
+      {
+        caption: 'Not Contains',
+        name: 'doesNotContain',
+      },
+      {
+        caption: 'Is Empty',
+        name: 'isEmpty',
+      },
+      {
+        caption: 'Is Not Empty',
+        name: 'isNotEmpty',
+      },
+      {
+        caption: 'Starts With',
+        name: 'startswith',
+      },
+      {
+        caption: 'Ends With',
+        name: 'endsWith',
+      },
+      {
+        caption: 'Is',
+        name: 'is',
+      },
+      {
+        caption: 'Is Not',
+        name: 'isnot',
+      },
+    ],
+  },
+]
+
+export const groups = [
+  {
+    caption: 'And',
+    name: 'and',
+  },
+  {
+    caption: 'Or',
+    name: 'or',
+  },
+]
+
+export const filter = {
+  groupName: 'and',
+  items: [
+    {
+      field: 'campaign',
+      operator: 'isNotEmpty',
+    },
+  ],
+}
+
+export const columns = [
+  {
+    key: 'edit',
+    style: { width: 20 },
+    visible: true,
+  },
+  {
+    key: 'selection-cell',
+    style: { width: 80 },
+    visible: true,
+  },
+  {
+    key: 'sl',
+    title: 'SL',
+    dataType: DataType.Number,
+    style: { width: 100 },
+    visible: false,
+  },
+  {
+    key: 'campaign',
+    title: 'Campaign',
+    dataType: DataType.String,
+    style: { width: 240 },
+    visible: true,
+  },
+  {
+    key: 'market_id',
+    title: 'Market',
+    dataType: DataType.String,
+    style: { width: 350 },
+    visible: true,
+  },
+  {
+    key: 'state',
+    title: 'State',
+    dataType: DataType.String,
+    style: { width: 200 },
+    visible: true,
+  },
+  {
+    key: 'call_type',
+    title: 'Call Type',
+    dataType: DataType.String,
+    style: { width: 160 },
+    visible: true,
+  },
+  {
+    key: 'ranks',
+    title: 'Rank',
+    dataType: DataType.String,
+    style: { width: 100 },
+    visible: true,
+  },
+  {
+    key: 'nielsen_households',
+    title: 'Nielsen Households',
+    dataType: DataType.String,
+    style: { width: 240 },
+    visible: true,
+  },
+
+  {
+    key: 'start_date',
+    title: 'Start Date',
+    dataType: DataType.String,
+    style: { width: 200 },
+    visible: true,
+  },
+]
