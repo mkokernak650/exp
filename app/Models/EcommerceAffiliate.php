0ea4518ae37de433da46bd56a7ad4dd07ba441af
Fahim Sakib <fsakib991@gmail.com>
2023-01-16 15:20:39 +0600

    Fix: Ecommerce Affiliate - affiliate_fee_type

--- app/Models/EcommerceAffiliate.php
+++ app/Models/EcommerceAffiliate.php
@@ -21,6 +21,7 @@ class EcommerceAffiliate extends Model
         'order_type',
         'coupon_code',
         'dialed',
+        'affiliate_fee_type',
         'revenue',
         'affiliate_fee',
         'percentage',
