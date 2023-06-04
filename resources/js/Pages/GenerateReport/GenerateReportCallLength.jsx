85eb5aeb0eacb5861f786984491ea69fc9da9885
Fahim Sakib <fsakib991@gmail.com>
2023-06-04 12:06:35 +0600

    Generate Report Call Length - file name changing

--- resources/js/Pages/GenerateReport/GenerateReportCallLength.jsx
+++ resources/js/Pages/GenerateReport/GenerateReportCallLength.jsx
@@ -279,15 +279,24 @@ const GenerateReportAffiliate = () => {
     return affiliateNames
   }
 
-  const fileName = `${values?.type ? values.type : ''}_CallLength_Report${values?.market ? `_For_Markets(${values.market})` : ''
+  const fileName = `${values?.type ? values.type : ''}_CallLength${values?.customer_name ? `_(${values.customer_name})` : ''}${values?.affiliate_id ? `_(${getAffiliateNames().toString()})` : ''
+    }${values?.campaign ? `_(${getCampaignNames(values.campaign).toString()})` : ''}${values?.market ? `_(${values.market})` : ''
+    }${year?.year ? `_(${year.year.toString()})` : ''
+    }${(values?.start_date && !year?.year)
+      ? `_(${dateFormat(values.start_date)}_To_${dateFormat(values?.end_date)})`
+      : ''
+    }`
+  values.file_name = fileName
+
+  const fileNameForEmailCriteria = `${values?.type ? values.type : ''}_CallLength_Report${values?.market ? `_For_Markets(${values.market})` : ''
     }${values?.customer_name ? `_For_Customers(${values.customer_name})` : ''}${values?.annotation ? `_For_Annotations(${values.annotation})` : ''
     }${values?.campaign ? `_For_Campaigns(${getCampaignNames(values.campaign).toString()})` : ''}${values?.affiliate_id ? `_For_Affiliates(${getAffiliateNames().toString()})` : ''
     }${values?.target_name ? `_For_Targets(${values.target_name.toString()})` : ''}${year?.year ? `_For_Years(${year.year.toString()})` : ''
-    }${values?.start_date
+    }${(values?.start_date && !year?.year)
       ? `_For_Date_Range(${dateFormat(values.start_date)}_To_${dateFormat(values?.end_date)})`
       : ''
     }_Created@${currentDate()}`
-  values.file_name = fileName
+  values.fileNameForEmailCriteria = fileNameForEmailCriteria
 
   const handleSubmit = () => {
     setLoading(true)
