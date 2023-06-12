87b26407df0bbb8c664a64a43a76a046201ad8f9
Fahim Sakib <fsakib991@gmail.com>
2023-06-12 11:25:16 +0600

    Generate Report Affiliate - date range added to header

--- app/Http/Controllers/ReportGeneratorController.php
+++ app/Http/Controllers/ReportGeneratorController.php
@@ -662,7 +662,19 @@ class ReportGeneratorController extends Controller
             $year = $request->year;
         }
 
-        $header = $this->getHeader($request->type, $campaign);
+        if (!empty($request->year)) {
+            $selectedYears      = implode(', ', $request->year);
+            $yearNaming         = (count($request->year) > 1) ? 'Years' : 'Year';
+            $dateRangeForHeader = [$yearNaming => $selectedYears];
+        } elseif (isset($request->start_date) && isset($request->end_date)) {
+            $dateRangeStart     = date_format(date_create($request->start_date), 'd-M-Y');
+            $dateRangeEnd       = date_format(date_create($request->end_date), 'd-M-Y');
+            $dateRangeForHeader = ['Date Range' => $dateRangeStart . ' To ' . $dateRangeEnd];
+        } else {
+            $dateRangeForHeader = [];
+        }
+
+        $header = $this->getHeader($request->type, $campaign, $dateRangeForHeader);
 
         $archived = [];
         $call_summary = [];
@@ -1395,7 +1407,7 @@ class ReportGeneratorController extends Controller
         return DB::select($sql);
     }
 
-    protected function getHeader($type, $campaignName)
+    protected function getHeader($type, $campaignName, $dateRange = [])
     {
         $header       = [];
         $userFullName = auth()->user()->firstname . ' ' . auth()->user()->lastname;
@@ -1413,6 +1425,14 @@ class ReportGeneratorController extends Controller
                 $header['Campaign'] .= ', ';
             }
             $header['Campaign'] .= $campaignName;
+        }
+
+        if (!empty($dateRange)) {
+            foreach ($dateRange as $key => $value) {
+                $header[$key] = $value;
+            }
+        } else {
+            $header['Date Range'] = '';
         }
 
         $header['Prepared by'] = $userFullName;
