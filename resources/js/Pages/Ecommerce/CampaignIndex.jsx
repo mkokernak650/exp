317b1e49226c551707741948d28269f3d9adc4b2
Fahim Sakib <fsakib991@gmail.com>
2023-06-13 17:21:46 +0600

    ecommerce campaign- columnData restored

--- resources/js/Pages/Ecommerce/CampaignIndex.jsx
+++ resources/js/Pages/Ecommerce/CampaignIndex.jsx
@@ -84,7 +84,10 @@ const CampaignIndex = () => {
   )
 
   const tablePropsInit = {
-    columns:columns,
+    columns:
+      columnsData.length && JSON.parse(columnsData[0])?.[optionKey]
+        ? JSON.parse(columnsData[0])?.[optionKey]
+        : columns,
     paging: {
       enabled: true,
       pageIndex: 0,
