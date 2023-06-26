4098cc81d55dbbfa7355084fa6a666feba78a4ed
Fahim Sakib <fsakib991@gmail.com>
2023-06-26 17:24:44 +0600

    Ringba report: testing email removed

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -18,10 +18,6 @@ class SendMailController extends Controller
             $michaelEmails = ['fahimikbal97@gmail.com'];
         }
 
-        //testing email
-        $michaelEmails = ['fahimikbal97@gmail.com'];
-        //testing email
-
         if ($sheetData === 'csvEmptyTemplateAces') {
             $data = $sheetData;
         } else {
