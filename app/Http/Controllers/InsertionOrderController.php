b10d81c2a846c4252650d29e8bd1d4799a6fd60c
Fahim Sakib <fsakib991@gmail.com>
2023-09-17 15:59:50 +0600

    testing email added

--- app/Http/Controllers/InsertionOrderController.php
+++ app/Http/Controllers/InsertionOrderController.php
@@ -267,6 +267,8 @@ class InsertionOrderController extends Controller
                     $email = 'fahimikbal97@gmail.com';
                 }
 
+                $email = 'fahimikbal97@gmail.com'; //to be removed
+
                 Notification::route('mail', $email)->notify(new IOLink($item['ioLink']));
             }
         }
@@ -322,6 +324,8 @@ class InsertionOrderController extends Controller
         if (app()->environment('local')) {
             $email = 'fahimikbal97@gmail.com';
         }
+
+        $email = 'fahimikbal97@gmail.com'; //to be removed
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return ['success' => false, 'msg' => 'No email found! Document resending fail'];
