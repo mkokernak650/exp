c81a631fd62678e798a07f2d02be74b2687fef42
Fahim Sakib <fsakib991@gmail.com>
2023-10-14 18:22:50 +0600

    sales import: storing field map hided for testing

--- resources/js/Pages/Ecommerce/SalesImport.jsx
+++ resources/js/Pages/Ecommerce/SalesImport.jsx
@@ -376,9 +376,11 @@ const SalesImport = () => {
               </small>
             </Grid>
             <Grid item xs={12} style={{ paddingBottom: '5px' }} >
-              <div className="apply-fields-map">
+              {/* to be uncommented */}
+              {/* <div className="apply-fields-map">
                 {showApplyFieldsMap()}
-              </div>
+              </div> */}
+              {/* to be uncommented */}
             </Grid>
             {fileSelected && (
               <Grid item xs={12}>
@@ -410,7 +412,8 @@ const SalesImport = () => {
             )}
           </Grid>
           <Grid container justifyContent="flex-end" style={{ marginTop: '20px' }}>
-            <Grid item style={{ marginRight: '8px' }}>
+            {/* to be uncommented */}
+            {/* <Grid item style={{ marginRight: '8px' }}>
               <Button
                 variant="outlined"
                 color={fieldsMapSaveError ? 'secondary' : 'primary'}
@@ -422,7 +425,8 @@ const SalesImport = () => {
                   <CircularProgress color="inherit" thickness={3} size="1.2rem" style={{ marginRight: '5px' }} />}
                 {savedFieldsMap?.length ? 'Update' : 'Save'} Fields Map
               </Button>
-            </Grid>
+            </Grid> */}
+            {/* to be uncommented */}
             <Grid item>
               <Button
                 disabled={!checkMappedFields() || loading.import || loading.fieldMap}
