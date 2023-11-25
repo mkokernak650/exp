e61d49b41666809817396e8a6f8c1eaa3c225581
FahimSakib <fsakib991@gmail.com>
2023-11-25 19:27:09 +0600

    IO - view, public, doc: terms and conditions (updated)

--- resources/views/insertion-order/insertion-order-document.blade.php
+++ resources/views/insertion-order/insertion-order-document.blade.php
@@ -126,7 +126,8 @@
                             </ul>
                         </td>
                         <td style="width:40%">
-                            {{ $ioFor == 'customer' ? 'Customer ' : '' }}Insertion Order NO: {{ $billingDetails['ioNo'] }}
+                            {{ $ioFor == 'customer' ? 'Customer ' : '' }}Insertion Order NO:
+                            {{ $billingDetails['ioNo'] }}
                             {{ $billingDetails['status'] == 'canceled' ? ' (Canceled)' : '' }}
                         </td>
                     </tr>
@@ -184,25 +185,56 @@
             </div>
         </div>
         <div class="io-terms">
-            <p>{{$ioFor === 'customer' ? 'Customer' : 'ConsumerEXP'}} pays according to terms of this insertion order. {{$ioFor === 'customer' ? 'Customer may need to' : 'The company can'}} provide agency of record (AOR) proof upon request.</p>
-            <p>{{$ioFor === 'customer' ? 'Customer' : 'ConsumerEXP'}} pays via ACH bank processing according to periodic sales reports to media outlet. {{$ioFor === 'customer' ? 'Customer may provide an advance payment' : ''}}</p>
-            <p>
-                ConsumerEXP will provide {{$ioFor === 'customer' ? 'Customer' : 'media outlet'}} log-in access to its vendor banking portal to view and download detailed bills, call or order logs,
-                and track payments. Also, the {{$ioFor === 'customer' ? 'Customer' : 'vendor'}} portal will provide consolidated statements of accounts and contain uploaded transaction and sales documents.
-            </p>
-            <p>
-                ConsumerEXP represents that it has required the companies that own the TV commercial(s) that they have licensed the images, spokespeople, and
-                music for the TV commercial(s).Furthermore, ConsumerEXP has required the companies that own the TV commercial(s) contained in this insertion
-                order to attest in its agreement with ConsumerEXP that the TV commercial(s) do not knowingly violate the rights of any individual, company,
-                state laws, or federal laws.
-            </p>
-            <p>
-                ConsumerEXP agrees to indemnify and hold media outlet harmless from any claims for damages (including reasonable attorney fees) based upon a claim
-                that a commercial run by ConsumerEXP violates applicable federal or state law.
-            </p>
-            <p>ConsumerEXP and media outlet agree that insertion order, or titles in the insertion order, can be cancelled with two weeks advance notice.
-                {{$ioFor === 'customer' ? ' Customer may be charged for dubs if they cannot supply dubs.' : ''}}
-            </p>
+            {!! $ioFor === 'customer'
+                ? '<p>Customer pays on a per order or per call rate according to terms of this insertion order.</p>
+                        <p>Customer pays via ACH bank processing according to periodic invoices generated or advance payments.
+                        </p>
+                        <p>Customer may provide an advance payment or retainer agreement depending upon agreement.</p>
+                        <p>
+                            Customer may be charged for dubs, for an agreed upon rate, ( if they cannot supply dubs to the
+                            affiliates).
+                            Dub rate will include traffic charges and may include assignment of product codes and/or telephone
+                            numbers.
+                        </p>
+                        <p>
+                            ConsumerEXP will provide Customer log-in access to its vendor banking portal to view and download
+                            detailed
+                            bills, call or order logs, and track payments. Also, the Customer portal will provide consolidated
+                            statements
+                            of accounts and contain uploaded transaction and sales documents.
+                        </p>
+                        <p>
+                            Customer states that it owns the TV commercial(s) and that it has licensed the images, spokespeople,
+                            and music for the TV commercial(s). Furthermore, Customer attests that the TV commercial(s) do not
+                            knowingly violate the rights of any individual, company, state laws, or federal laws.
+                        </p>
+                        <p>Customer and ConsumerEXP can cancel the flight according to the terms of this insertion order.</p>'
+                : '<p>
+                            ConsumerEXP pays according to terms of this insertion order.
+                            The company can provide agency of record (AOR) proof upon request.</p>
+                        <p>ConsumerEXP may provide a URL link to the TV commercial in this insertion order or by separate email.
+                        </p>
+                        <p>ConsumerEXP pays via ACH bank processing according to periodic sales reports to media outlet.</p>
+                        <p>
+                            ConsumerEXP will provide media outlet log-in access to its vendor banking portal to view and
+                            download detailed bills, call or order logs, and track payments.
+                        </p>
+                        <p>
+                            ConsumerEXP represents that it has required the companies that own the TV commercial(s) that they
+                            have licensed the images, spokespeople, and music for the TV commercial(s).Furthermore,
+                            ConsumerEXP has required the companies that own the TV commercial(s) contained in this insertion
+                            order to attest in its agreement with ConsumerEXP that the TV commercial(s) do not knowingly
+                            violate the rights of any individual, company, state laws, or federal laws.
+                        </p>
+                        <p>
+                            ConsumerEXP agrees to indemnify and hold media outlet harmless from any claims for damages
+                            (including reasonable attorney fees) based upon a claim that a commercial run by ConsumerEXP
+                            violates applicable federal or state law.
+                        </p>
+                        <p>
+                            ConsumerEXP and media outlet agree that insertion order, or titles in the insertion order,
+                            can be cancelled with two weeks advance notice.
+                        </p>' !!}
         </div>
         <div class="io-footer">
             <p>650 Huntington Avenue, Floor 22M | Boston, MA 02115 | Phone/Text: 617-874-4247 | www.consumerexp.com</p>
