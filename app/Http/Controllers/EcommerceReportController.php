decab81eca0bb3e7ce15aeb317b0820225023144
FahimSakib <fsakib991@gmail.com>
2023-12-02 18:09:46 +0600

    ER: total fee/affiliate fee sign

--- app/Http/Controllers/EcommerceReportController.php
+++ app/Http/Controllers/EcommerceReportController.php
@@ -1294,12 +1294,12 @@ class EcommerceReportController extends Controller
                 $totalFee = $salesData->sum(function ($item) {
                     return (float) $item->{'Total Fee'};
                 });
-                $reportOn .= "<br> <p style='font-size: 18px;'><strong>Total Fee: {$totalFee}</strong></p>";
+                $reportOn .= '<br> <p style="font-size: 18px;"><strong>Total Fee: $' . $totalFee . '</strong></p>';
             } else {
                 $affiliateFee = $salesData->sum(function ($item) {
                     return (float) $item->{'Affiliate Fee'};
                 });
-                $reportOn .= "<br> <p style='font-size: 18px;'><strong>Affiliate Fee: {$affiliateFee}</strong></p>";
+                $reportOn .= '<br> <p style="font-size: 18px;"><strong>Affiliate Fee: $' . $affiliateFee . '</strong></p>';
             }
         }
 
