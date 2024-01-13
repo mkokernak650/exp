5d6202b5c7f56539a67bee1c66d8c50edbe63b2b
FahimSakib <fsakib991@gmail.com>
2024-01-13 21:29:49 +0600

    send campaign - email body message.init

--- app/Http/Controllers/SendCampaignController.php
+++ app/Http/Controllers/SendCampaignController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Affiliate;
 use App\Models\Campaign;
 use App\Models\EcommerceCampaign;
+use App\Notifications\SendCampaign;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 use Inertia\Inertia;
 
 class SendCampaignController extends Controller
@@ -29,13 +31,43 @@ class SendCampaignController extends Controller
         $additionalEmails   = explode(',', $request['additionalEmails']);
         $emails             = array_merge($affiliatesEmail, $additionalEmails);
         $emails             = array_unique($emails);
+        $ringbaCampaigns    = Campaign::active()->get();
+        $ecommerceCampaigns = EcommerceCampaign::active()->get();
+        $allCampaigns       = $ringbaCampaigns->merge($ecommerceCampaigns)->sortByDesc('created_at')->values();
+        $message            = '';
 
-        dd($emails);
+        foreach ($allCampaigns as $campaign) {
+            // varDump($campaign);
+            // dd($campaign);
+            if (!empty($campaign->description)) {
+                // $description = str_replace('\n', '<br>', $campaign->description);
+                $description = $campaign->description;
+            } else {
+                $description = null;
+            }
 
-        dd($additionalEmails);
-        dd($affiliatesEmail);
+            if (!empty($campaign->length_url)) {
+                // $lengthUrl = str_replace('\n', '<br>', $campaign->length_url);
+                $lengthUrl = $campaign->length_url;
+            } else {
+                $lengthUrl = null;
+            }
 
+            $message .= '<div><strong>' . $campaign->campaign_name . '</strong>' . (!empty($description) ? "<br>{$description}<br>" : '') . (!empty($lengthUrl) ? "<br>{$lengthUrl}<br>" : '') .
 
-        dd($selectedAffiliates);
+                '</div><br>';
+        }
+
+        if (app()->environment('local')) {
+            $emails = ['fahimikbal97@gmail.com'];
+        }
+
+        foreach ($emails as $email) {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                Notification::route('mail', $email)->notify(new SendCampaign($message));
+            }
+        }
+
+        dd($message);
     }
 }
