a57952f08a3fd96745ff3bcd0156d5760423c6c7
Shakhawat Hosen <mdshakhawathosen122@gmail.com>
2026-05-05 11:27:06 +0600

    feat: add favicon to the application

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+
   <title inertia>{{ config('app.name', 'Laravel') }}</title>
   @vite('resources/sass/app.scss')
   @routes
